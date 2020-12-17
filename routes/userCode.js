e88d8488af1d4fb3d95fb74014d380d136111ee9
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-12-17 17:37:02 +1100

    Changed response format for the '/' POST route

--- routes/userCode.js
+++ routes/userCode.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
   app.post('/', (req, res) => {
     console.log(req.body.code);
-    res.status('201').send(req.body.code);
+    res.status('201').send({
+      data: req.body.code,
+      message: 'code added to database',
+    });
   });
 };
