99d0cdf47fac415341f42be2a88d598a0b09f57a
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-12-21 18:45:29 +1100

    Added basic DB Schema

---
+++ config/schema.js
@@ -0,0 +1,30 @@
+{
+  users: [
+    {
+      userID: '',
+      projects: [
+        {
+          projectID: '',
+          userCode: [
+            {
+              fileID: '',
+              fileName: '',
+              version: '',
+              content: '',
+            },
+          ],
+          buildFiles: [
+            {
+              fileID: '',
+              fileName: '',
+              version: '',
+              content: '',
+              userCodeRefs: [{ filename: '', version: '' }],
+            },
+          ],
+          analysis: [{}],
+        },
+      ],
+    },
+  ];
+}
