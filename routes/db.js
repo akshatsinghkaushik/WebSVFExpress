8c5bf397c75c81a4d3169b6f8e0449a1ca36bf2e
akshatsinghkaushik <akshatsingh99@gmail.com>
2021-01-06 18:26:20 +1100

    Added folder check in /saveFile route and added /saveFolder route

--- routes/db.js
+++ routes/db.js
@@ -8,12 +8,148 @@ module.exports = (app) => {
       'projects.projectID': 'abc',
     });
 
+    //If the project does not exist respond to the req with status 404
+    if (!projectFound) {
+      res.status('404').send({
+        message: 'Project Not Found',
+      });
+    }
+    //If the project is found (core logic for this endpoint)
+    else {
+      //Check if the folder being sent already exists in the db if not create folder first
+      const foundFolder = await userSchema.exists({
+        googleID: 'abcd',
+        'projects.projectID': 'abc',
+        'projects.userCode.folderName': req.body.folderName,
+      });
+
+      if (foundFolder) {
+        //Check if the file being sent already exists in the db
+        const foundFile = await userSchema.exists({
+          googleID: 'abcd',
+          'projects.projectID': 'abc',
+          'projects.userCode.folderName': req.body.folderName,
+          'projects.userCode.files.fileName': req.body.fileName,
+        });
+
+        //If a file being sent through the req already exists
+        if (foundFile) {
+          //Get the existing file file from the db
+          const file = await userSchema.findOne({
+            googleID: 'abcd',
+            'projects.projectID': 'abc',
+            'projects.userCode.folderName': req.body.folderName,
+            'projects.userCode.files.fileName': req.body.fileName,
+          });
+
+          //Find the index of the project in the projects array of the user
+          const projectIndex = file.projects.findIndex((project) => {
+            return project.projectID === 'abc';
+          });
+          //Find the index of the file in the userCode array of the current project
+          const codeFolderIndex = file.projects[
+            projectIndex
+          ].userCode.findIndex((code) => {
+            return code.folderName === req.body.folderName;
+          });
+
+          const codeFileIndex = file.projects[projectIndex].userCode[
+            codeFolderIndex
+          ].files.findIndex((code) => {
+            return code.fileName === req.body.fileName;
+          });
+
+          //Calculate a new version for the file to be stored as a parameter in the userCode file being updated
+          const newVersion = (
+            Number(
+              file.projects[projectIndex].userCode[codeFolderIndex].files[
+                codeFileIndex
+              ].version
+            ) + 0.01
+          ).toFixed(2);
+
+          //Update the file with the new code sent through the req and update its versioning
+          await userSchema.findOneAndUpdate(
+            {
+              googleID: 'abcd',
+            },
+            {
+              $set: {
+                'projects.$[i].userCode.$[j].files.$[k]': {
+                  fileName: req.body.fileName,
+                  version: newVersion,
+                  fileID: 'xyz',
+                  content: req.body.code,
+                },
+              },
+            },
+            {
+              arrayFilters: [
+                { 'i.projectID': 'abc' },
+                { 'j.folderName': req.body.folderName },
+                { 'k.fileName': req.body.fileName },
+              ],
+            }
+          );
+
+          //Respond to the request with status 201 with the code sent through the req and a message
+          res.status('201').send({
+            data: req.body.code,
+            message: 'code updated in database',
+          });
+        }
+        //If the file doesn't exist in the db
+        else {
+          //Add file to the db
+          await userSchema.findOneAndUpdate(
+            {
+              googleID: 'abcd',
+            },
+            {
+              $addToSet: {
+                'projects.$[h].userCode.$[i].files': {
+                  fileName: req.body.fileName,
+                  version: '1.0',
+                  fileID: 'xyz',
+                  content: req.body.code,
+                },
+              },
+            },
+            {
+              arrayFilters: [
+                { 'h.projectID': 'abc' },
+                { 'i.folderName': req.body.folderName },
+              ],
+              upsert: true,
+            }
+          );
+
+          //Respond to the request with status 201 with the code sent through the req and a message
+          res.status('201').send({
+            data: req.body.code,
+            message: 'code added to database',
+          });
+        }
+      } else {
+        res.status('201').send({
+          message: 'folder not found: Create the folder first then try again',
+        });
+      }
+    }
+  });
+
+  app.post('/db/saveFolder/', async (req, res) => {
+    //Check if the project exists in the current user's collection
+    const projectFound = await userSchema.exists({
+      googleID: 'abcd',
+      'projects.projectID': 'abc',
+    });
+
     //Check if the file being sent already exists in the db
-    const foundFile = await userSchema.exists({
+    const foundFolder = await userSchema.exists({
       googleID: 'abcd',
       'projects.projectID': 'abc',
       'projects.userCode.folderName': req.body.folderName,
-      'projects.userCode.files.fileName': req.body.fileName,
     });
 
     //If the project does not exist respond to the req with status 404
@@ -25,69 +161,10 @@ module.exports = (app) => {
     //If the project is found (core logic for this endpoint)
     else {
       //If a file being sent through the req already exists
-      if (foundFile) {
-        //Get the existing file file from the db
-        const file = await userSchema.findOne({
-          googleID: 'abcd',
-          'projects.projectID': 'abc',
-          'projects.userCode.folderName': req.body.folderName,
-          'projects.userCode.files.fileName': req.body.fileName,
-        });
-
-        //Find the index of the project in the projects array of the user
-        const projectIndex = file.projects.findIndex((project) => {
-          return project.projectID === 'abc';
-        });
-        //Find the index of the file in the userCode array of the current project
-        const codeFolderIndex = file.projects[projectIndex].userCode.findIndex(
-          (code) => {
-            return code.folderName === req.body.folderName;
-          }
-        );
-
-        const codeFileIndex = file.projects[projectIndex].userCode[
-          codeFolderIndex
-        ].files.findIndex((code) => {
-          return code.fileName === req.body.fileName;
-        });
-
-        //Calculate a new version for the file to be stored as a parameter in the userCode file being updated
-        const newVersion = (
-          Number(
-            file.projects[projectIndex].userCode[codeFolderIndex].files[
-              codeFileIndex
-            ].version
-          ) + 0.01
-        ).toFixed(2);
-
-        //Update the file with the new code sent through the req and update its versioning
-        await userSchema.findOneAndUpdate(
-          {
-            googleID: 'abcd',
-          },
-          {
-            $set: {
-              'projects.$[i].userCode.$[j].files.$[k]': {
-                fileName: req.body.fileName,
-                version: newVersion,
-                fileID: 'xyz',
-                content: req.body.code,
-              },
-            },
-          },
-          {
-            arrayFilters: [
-              { 'i.projectID': 'abc' },
-              { 'j.folderName': req.body.folderName },
-              { 'k.fileName': req.body.fileName },
-            ],
-          }
-        );
-
+      if (foundFolder) {
         //Respond to the request with status 201 with the code sent through the req and a message
         res.status('201').send({
-          data: req.body.code,
-          message: 'code updated in database',
+          message: 'folder already exists in database',
         });
       }
       //If the file doesn't exist in the db
@@ -99,19 +176,14 @@ module.exports = (app) => {
           },
           {
             $addToSet: {
-              'projects.$[h].userCode.$[i].files': {
-                fileName: req.body.fileName,
-                version: '1.0',
-                fileID: 'xyz',
-                content: req.body.code,
+              'projects.$[h].userCode': {
+                folderID: Math.random().toString(),
+                folderName: req.body.folderName,
               },
             },
           },
           {
-            arrayFilters: [
-              { 'h.projectID': 'abc' },
-              { 'i.folderName': req.body.folderName },
-            ],
+            arrayFilters: [{ 'h.projectID': 'abc' }],
             upsert: true,
           }
         );
@@ -119,7 +191,7 @@ module.exports = (app) => {
         //Respond to the request with status 201 with the code sent through the req and a message
         res.status('201').send({
           data: req.body.code,
-          message: 'code added to database',
+          message: 'folder added to database',
         });
       }
     }
